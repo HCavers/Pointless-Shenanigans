ce811e910849f5d4eb028977c1096d8de4d5e513
HCavers <huntercavers@xtra.co.nz>
2017-08-30 14:34:29 +1200

    I made it not so bad

--- thingy.java
+++ thingy.java
@@ -2,6 +2,6 @@ class thingy
 {
 	public static void main(String[] args)
 	{
-		System.out.println("We are all going to die :P");
+		System.out.println("We are all going to die :P, just kidding :)");
 	}
 }
